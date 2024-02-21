65c0cf046e65c81df7c572dbf55febaa70bf2707
Jamseer007 <jamseer007@gmail.com>
2024-02-22 00:05:43 +0530

    Updated the invoice data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -26,7 +26,7 @@ export async function fetchRevenue() {
     // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
-  } catch (error) {
+  } catch (error) { 
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
   }
